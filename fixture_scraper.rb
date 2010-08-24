6284386944802638588a1c0c71dd9f5377046658
Kevin Hughes <kevtufc@gmail.com>
2010-08-24 21:30:26 +0100

    Make it run as a WEBrick server and return the calendar over that

--- fixture_scraper.rb
+++ fixture_scraper.rb
@@ -1,41 +1,96 @@
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
+require 'optparse'
+require 'webrick'
+include WEBrick
 
-# Simply set this to the page your team's fixtures are on, and run the script
-page = "http://www.torquayunited.com/page/Fixtures/0,,10445,00.html"
+# Change this to the url of the fixtures page you're after,
+# run the server and point your calendar import at it.
+url = ("http://www.torquayunited.com/page/Fixtures/0,,10445,00.html"
 
-doc = open(page) { |f| Hpricot(f) }
-
-puts "BEGIN:VCALENDAR"
-puts "VERSION:2.0"
-
-month = nil 
-
-team = (doc/"head/title").inner_html.split("|").first.gsub!(/\s*$/,"")
-
-(doc/"table.fixtureList")./("tr").each do |row|
-  next if row.attributes['class'] == ""
-  if row.attributes['class'] == "rowHeader"
-    month = Date::MONTHNAMES.index(month = (row/"td").inner_html)
-  else
-    day        = (row/"td[1]").inner_html.match("[1234567890]+")[0].to_i
-    time       = (row/"td[2]").inner_html.gsub(/:/,"")
-    opponents  = (row/'td[4]/a').inner_html
-    home       = (row/'td[3]').inner_html=="H"
-    comp       = (row/'td[5]').inner_html.split.first
-    (hscore,
-     ascore)   = (row/'td[7]').inner_html.gsub(/[^\-0-9]/,"").split("-")
-    puts "BEGIN:VEVENT"
-    puts "DTSTART:201#{month > 6 ? "0" : "1"}#{"%02d" % month}#{"%02d" % day}T#{time}00Z"
-    puts "DTEND:201#{month > 6 ? "0" : "1"}#{"%02d" % month}#{"%02d" % day}T#{time.to_i+200}00Z"
-    if home
-      puts "SUMMARY:#{team} #{hscore} v #{ascore} #{opponents} (#{comp})"
-    else
-      puts "SUMMARY:#{opponents} #{ascore} v #{hscore} #{team} (#{comp})"
-    end
-    puts "END:VEVENT" 
+class Fixture
+  attr_accessor :start,:home, :away, :hscore, :ascore, :comp
+  def to_vcalendar
+    out  = "BEGIN:VEVENT\n"
+    out += "DTSTART:#{start}\n"
+    out += "DTEND:#{start + 7200}\n"
+    out += "SUMMARY:#{home}"
+    out += " " + hscore.to_s if hscore
+    out += " v "
+    out += ascore.to_s + " " if ascore
+    out += "#{away} (#{comp})\n"
+    out += "END:VEVENT\n" 
   end
 end
 
-puts "END:VCALENDAR"
+class FixtureList
+  attr_accessor :fixtures
+
+  def initialize(url)
+    @url = url
+    @fixtures = []
+  end
+ 
+  def parse
+    doc = open(@url) { |f| Hpricot(f) }
+    month = nil 
+    team = (doc/"head/title").inner_html.split("|").first.strip
+    (doc/"table.fixtureList")./("tr").each do |row|
+      next if row.attributes['class'] == ""
+      if row.attributes['class'] == "rowHeader"
+        month = Date::MONTHNAMES.index(month = (row/"td").inner_html)
+      else
+        game = Fixture.new
+        day             = (row/"td[1]").inner_html.match("[1234567890]+")[0].to_i
+        (hour,
+	 min)           = (row/"td[2]").inner_html.split(/:/)
+        game.start      = Time.utc(month > 6 ? 2010 : 2011,month,day,hour,min)
+        opponents       = (row/'td[4]/a').inner_html.strip
+        game.comp       = (row/'td[5]').inner_html.split.first
+        home            = (row/'td[3]').inner_html=="H"
+	if home
+          (game.hscore,
+	   game.ascore) = (row/'td[7]').inner_html.gsub(/[^\-0-9]/,"").split("-").map{|s| s.to_i}
+	   game.home    = team
+	   game.away    = opponents
+	else
+          (game.ascore,
+	   game.hscore) = (row/'td[7]').inner_html.gsub(/[^\-0-9]/,"").split("-").map{|s| s.to_i}
+	   game.away    = team
+	   game.home    = opponents
+	end
+	@fixtures << game
+      end
+    end
+  end
+
+  def to_vcalendar
+    out = "BEGIN:VCALENDAR\n"
+    out += "VERSION:2.0\n"
+    out += @fixtures.map{|f| f.to_vcalendar}.join
+    out += "END:VCALENDAR\n"
+  end
+
+  def count
+    @fixtures.size
+  end
+
+end
+
+
+class FixtureServlet < HTTPServlet::AbstractServlet
+  def do_GET(req,resp)
+    f=FixtureList.new(url)
+    f.parse
+    resp.body=f.to_vcalendar
+    raise HTTPStatus::OK
+  end
+end
+
+server = HTTPServer.new(:Port => 8001)
+server.mount "/", FixtureServlet
+['INT', 'TERM'].each {|signal| 
+    trap(signal) {server.shutdown}
+}
+server.start
